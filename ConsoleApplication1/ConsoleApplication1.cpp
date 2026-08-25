a6689fd6c27165afddd4b12dce15ca05eea577d9
4D-20\koyo <a>
2026-08-25 14:50:48 +0900

    特殊型

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,17 +1,5 @@
 ﻿#include <iostream>
 
-template<typename T , std::size_t N>
-
-class StaticArray {
-	T data[N];
-public:
-
-	constexpr T & operator[](std::size_t i)  {return data[i];}
-	constexpr const T& operator[](std::size_t i) const {return data[i];}
-	constexpr std::size_t size() const {return N;}
-
-};
-
 template <typename T> struct IsPointer {
 	static constexpr bool value = false;
 };
@@ -20,14 +8,15 @@ template <typename U> struct IsPointer<U*> {
 	static constexpr bool value = true;
 };
 
+struct NotPointer : IsPointer<int>{
+
+};
+
 int main() {
-	StaticArray<int, 30> data;
-	IsPointer<int> a;
+	std::cout<< IsPointer<int>::value << std::endl;
+	std::cout << IsPointer<int*>::value << std::endl;
+	std::cout << IsPointer<float>::value << std::endl;
+	std::cout << IsPointer<float*>::value << std::endl;
+	std::cout << NotPointer::value << std::endl;
 
-	std::cout << a.value << std::endl;
-
-	for (auto i = 0; i < data.size(); ++i) {
-		data[i] = i;
-	}
-	std::cout << data[5] << std::endl;
 }
