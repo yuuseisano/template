7043a8b3fec6f4b60f626ccafd2f27e39b325536
4D-20\koyo <a>
2026-08-25 13:38:45 +0900

    templateに置き換え

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,29 +1,19 @@
 ﻿#include <iostream>
 
-
-int max_int(int a, int b)
-{
-	return (a > b) ? a : b;
-}
-
-long max_long(long a, long b)
-{
-	return a > b ? a : b;
-}
-
-int max_float(float a, float b)
+template<class T>
+T max(T a, T b)
 {
 	return (a > b) ? a : b;
 }
 
 int main()
 {
-	float a = 10.1f;
-	float b = 10.2f;
+	float a = 10.1;
+	float b = 10.2;
 
-	std::cout << "max:" << max_int(a, b) << std::endl;
-	std::cout << "max:" << max_long(a, b) << std::endl;
-	std::cout << "max:" << max_float(a, b) << std::endl;
+	std::cout << "max:" << max(a, b) << std::endl;
+	std::cout << "max:" << max(a, b) << std::endl;
+	std::cout << "max:" << max(a, b) << std::endl;
 
 
 }
